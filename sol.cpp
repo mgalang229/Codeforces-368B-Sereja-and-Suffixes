c2d338d53eb755150a49c03329e640ab66e8ebd7
Mark Jason Galang <51401355+mgalang229@users.noreply.github.com>
2022-05-27 20:58:38 +0800

    Create sol.cpp

---
+++ sol.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void test_case() {
+	int n, m;
+	cin >> n >> m;
+	vector<int> a(n);
+	for(int i = 0; i < n; i++) {
+		cin >> a[i];
+	}
+	set<int> s;
+	vector<int> distinct(n, 0);
+	for(int i = n - 1; i >= 0; i--) {
+		if(s.find(a[i]) == s.end()) {
+			s.insert(a[i]);
+			distinct[i] = (int) s.size();
+		} else {
+			distinct[i] = distinct[i+1];
+		}
+	}
+	//for(int i = 0; i < n; i++) {
+		//cout << distinct[i] << " ";
+	//}
+	//cout << "\n";
+	for(int i = 0; i < m; i++) {
+		int l;
+		cin >> l;
+		cout << distinct[l-1] << "\n";
+	}
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	int T = 1;
+	//cin >> T;
+	for(int tc = 0; tc < T; tc++) {
+		test_case();
+	}
+}
